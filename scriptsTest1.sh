a81ae929254064ffe8089582778e1dc8623965c3
Mike J Hewitt <mikeh@electroteach.com>
2018-02-21 22:44:08 +1100

    just more

--- scriptsTest1.sh
+++ scriptsTest1.sh
@@ -1,5 +1,7 @@
 for i in grabbing hand2 left_ptr; do echo $i; done
+MY_NUMBER=4
+for i in 1 3 6 10 ; do echo $i * MY_NUMBER; done
 
-for i in 1 3 6 10 ; do echo $i; done
+grep "^${USER}:" /etc/passwd
 
 
