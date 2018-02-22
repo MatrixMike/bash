6f53af297e2d2f446955880e7a3ed52c469cec69
Mike J Hewitt <mikeh@electroteach.com>
2018-02-22 11:04:46 +1100

    after suggestions from ./shellcheck

--- scriptsTest1.sh
+++ scriptsTest1.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 for i in grabbing hand2 left_ptr; do echo $i; done
 MY_NUMBER=4
-for i in 1 3 6 10 ; do echo $i * MY_NUMBER; done
+for i in 1 3 6 10 ; do echo $i * MY_NUMBER ; done
 
 grep "^${USER}:" /etc/passwd
 
+echo MY_NUMBER
+echo $MY_NUMBER
 
--- scriptsTest2.sh
+++ scriptsTest2.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 for i in $(ls | grep xmc); do mv $i $(echo $i | sed 's/.xmc//g'); done
+# is this a comment?
 
